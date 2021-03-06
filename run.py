c9ec75b3bfcd8a5b3199ab2521037db5f6731a02
KimDoHyeon <pental@kakao.com>
2021-03-07 01:29:48 +0900

    Add user information

--- run.py
+++ run.py
@@ -1,21 +1,22 @@
+import os
+import re
 import sys
 from bs4 import BeautifulSoup
 from selenium import webdriver
-import os
-import re
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import uic
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import * 
-ui_path = os.getcwd() + "\login.ui"
+
+ui_path = os.getcwd() + "\src\login.ui"
 ui = uic.loadUiType(ui_path)[0] # Call ui file
 
 class LoginWindow(QMainWindow, ui):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        # self.setWindowIcon(QIcon('src/gui/icon.ico'))
+        self.setWindowIcon(QIcon('src\icon.ico'))
 
         # File Management Buttons
         self.login_button.clicked.connect(self.login)
@@ -32,6 +33,7 @@ class LoginWindow(QMainWindow, ui):
             options.add_argument('headless')
             options.add_argument('window-size=1920x1080')
             options.add_argument("disable-gpu")
+            global driver
             driver = webdriver.Chrome('chromedriver.exe', chrome_options=options)
             login_url = "https://cyber.jj.ac.kr/login.php"
             driver.get(login_url)
@@ -62,6 +64,7 @@ class LoginWindow(QMainWindow, ui):
                 QMessageBox.warning(self, '로그인 실패', '학번 또는 비밀번호를 확인해 주세요.', QMessageBox.Ok, QMessageBox.Ok)
             else :
                 QMessageBox.information(self, '로그인 성공', '모든 강의를 확인하기 때문에 시간이 소요될수 있습니다.', QMessageBox.Ok, QMessageBox.Ok)
+                
                 notice_url = "http://cyber.jj.ac.kr/local/ubnotification/"
                 driver.get(notice_url)
                 html = driver.page_source
@@ -85,11 +88,13 @@ class LoginWindow(QMainWindow, ui):
                     message = message.partition("<p>")[-1].replace("</p></div>","")
                     notice_value.append([name, timeago, message])
                 
+                global class_id
                 class_id = []
                 global class_detail
                 class_detail = []
                 for i in range(len(class_all)) :
                     class_id.append(class_all[i][1].split("?")[1][3:])
+
                 for i in range(len(class_id)) :
                     class_name = class_all[i][0]
                     class_process_url = "http://cyber.jj.ac.kr/report/ubcompletion/user_progress.php?id=" + class_id[i]
@@ -121,17 +126,38 @@ class LoginWindow(QMainWindow, ui):
                             break
                 self.management = MainWindow()
                 self.management.show()
-                
                 self.close()
-ui_main_path = os.getcwd() + "\main.ui"
+
+ui_main_path = os.getcwd() + "\src\main.ui"
 ui_main = uic.loadUiType(ui_main_path)[0]
 class MainWindow(QMainWindow, ui_main):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        # self.setWindowIcon(QIcon('res/icon.ico'))
+        self.setWindowIcon(QIcon('src\icon.ico'))
         self.notice_listWidget.itemDoubleClicked.connect(self.notice_chkItemDoubleClicked)
         self.class_listWidget.itemDoubleClicked.connect(self.class_chkItemDoubleClicked)
+
+        #Get User Infomormation
+        get_user_url = "http://cyber.jj.ac.kr/report/ubcompletion/user_progress.php?id=" + class_id[0]
+        driver.get(get_user_url)
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+
+        school_id = str(soup.select("#ubcompletion-progress-wrapper > div:nth-child(1) > table > tbody > tr:nth-child(1) > td"))
+        regex = re.compile('{}(.*){}'.format(re.escape('<td class="text-left">'), re.escape('</td>')))
+        school_id = regex.findall(school_id)[0]
+        self.school_number_line.setText(str(school_id))
+
+        name = str(soup.select("#ubcompletion-progress-wrapper > div:nth-child(1) > table > tbody > tr:nth-child(2) > td"))
+        regex = re.compile('{}(.*){}'.format(re.escape('<td class="text-left">'), re.escape('</td>')))
+        name = regex.findall(name)[0]
+        self.name_line.setText(str(name))
+
+        phone_number = str(soup.select("#ubcompletion-progress-wrapper > div:nth-child(1) > table > tbody > tr:nth-child(3) > td"))
+        regex = re.compile('{}(.*){}'.format(re.escape('<td class="text-left">'), re.escape('</td>')))
+        phone_number = regex.findall(phone_number)[0]
+        self.phone_number_line.setText(str(phone_number))
 
         for i in range(len(class_all)) :
             self.class_listWidget.addItem(class_all[i][0])
@@ -139,7 +165,6 @@ class MainWindow(QMainWindow, ui_main):
         for i in range(len(notice_value)) :
             self.notice_listWidget.addItem(notice_value[i][0])
 
-        #===
         _translate = QCoreApplication.translate
         self.tableWidget.setColumnCount(4)
         self.tableWidget.setRowCount(len(class_detail))
